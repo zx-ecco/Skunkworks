7d024a2176e8372b701d2ff27bb1fa8fb443b7ac
allnamesaretakeningithub <mnbvcxz@riseup.net>
2015-11-13 23:15:52 +0000

    Update xubuntu-14.04-desktop-install-fest.sh
    oi gajo

--- xubuntu-14.04-desktop-install-fest.sh
+++ xubuntu-14.04-desktop-install-fest.sh
@@ -1,4 +1,5 @@
 #receita para sistema com apps decentes/suporte crypto a partir de install Xubuntu 14.04.
+#abrir os repositórios restricted
 sudo sed -i 's/# deb/deb/g' /etc/apt/sources.list
 #remover as aplicações indesejáveis.
 sudo apt-get install --yes linux-image-generic linux-headers-generic linux-firmware linux-tools-generic
@@ -16,7 +17,8 @@ sudo apt-get --yes purge parole
 sudo apt-get --yes purge gmusicbrowser
 sudo apt-get --yes purge zeitgeist*
 sudo apt-get --yes autoremove 
-sudo echo "sistema limpo"
+sudo echo "sistema limpo" 
+sleep 2s
 #adicionar repositórios extra
 #adicionar o ppa do transmission
 sudo sh -c 'echo "deb http://ppa.launchpad.net/transmissionbt/ppa/ubuntu trusty main" > /etc/apt/sources.list.d/transmission-ppa-trusty.list'
@@ -49,6 +51,7 @@ sudo apt-get clean
 sudo apt-get --yes autoremove
 sudo apt-get install --yes mousepad
 sudo apt-get clean && sudo echo "Sistema Actualizado"
+sleep 2s
 #instalar the good stuff - core.
 #definir como aceite o eula das fonts da ms para evitar a pausa extra na instalação
 sudo echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true |sudo debconf-set-selections
@@ -92,3 +95,4 @@ sudo apt-get install --yes wine winetricks playonlinux
 sudo apt-get autoremove
 sudo apt-get clean
 sudo echo "Sistema Pronto"
+sleep 2s
